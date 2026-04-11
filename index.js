82e47aa86e391e26a91ae5980f4b3c1e2842e72e
Aditya Moorjmalani <adityamoorjmalani@Adityas-MacBook-Air.local>
2026-04-11 16:34:54 +0530

    ai model change

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const client = new OpenAI({
   apiKey: process.env.OPENAPIROUTERKEY,
 });
 
-const MODEL = "google/gemini-3-flash-preview";
+const MODEL = "nvidia/nemotron-3-nano-30b-a3b:free";
 
 const SYSTEM_PROMPT = `
 You are an AI assistant acting as a helpful travel agent.
